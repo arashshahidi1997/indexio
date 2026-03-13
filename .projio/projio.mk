7ba884db5fa91bde2ca70423dca61a71f490f00d
arash <A.Shahidi@campus.lmu.de>
2026-03-13 01:38:28 +0100

    url

--- .projio/projio.mk
+++ .projio/projio.mk
@@ -6,7 +6,7 @@ DATALAD ?= datalad
 PROJIO  ?= projio
 MSG     ?= Update
 
-.PHONY: save push
+.PHONY: save push url
 .PHONY: projio-init projio-config-user projio-config-show projio-status projio-auth
 .PHONY: projio-gh projio-gl projio-ria site-build site-serve site-stop site-list site-detect mcp
 
@@ -16,6 +16,9 @@ save:
 
 push:
 	$(DATALAD) push --to github
+
+url:
+	$(PROJIO) url -C .
 
 # --- Projio targets ---
 projio-init:
